f5d8747f96166febd7c6dfc594238a030315ec95
Martin Cheng <martincheng.dev@gmail.com>
2021-01-17 19:52:49 -0600

    DEV-1 Init config commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/setup/index.scss'
+import App from './modules/common/App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
